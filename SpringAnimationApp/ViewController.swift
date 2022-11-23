4c34479773dfc18b8019199f1f8fdc7103c69e67
Nikita Shirobokov <shirobokov97@yandex.ru>
2022-11-23 16:24:46 +0400

    Initial Commit

---
+++ SpringAnimationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpringAnimationApp
+//
+//  Created by Nikita Shirobokov on 23.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
